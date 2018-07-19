a5c1a61d1b19c7bcdb4b8215a07f7f1390f65f7f
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-07-19 01:22:13 -0300

    Tela de seleção da profundidade da busca

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 import pygame
-import base
 
 m, n = 9, 6
 
@@ -14,6 +13,38 @@ def main():
 
 	is_redPlayer = escolher_jogador()
 
+	depth = escolher_profundidade()
+
+	
+def escolher_profundidade():
+    surface.fill((0,0,0))
+    font = pygame.font.Font('Font.ttf', 18)
+    text = font.render("Qual a profundidade de busca?", 1, (100,100,100))
+    textpos = text.get_rect(centerx = 25*n, centery = 12*m)
+    surface.blit(text, textpos)
+    font = pygame.font.Font('Font.ttf', 48)
+    depth = 3
+    text = font.render(str(depth),1,(255,255,0))
+    textpos = text.get_rect(centerx = 25*n, centery = 25*m)
+    surface.blit(text, textpos)
+    pygame.display.update()
+
+    flag = True
+    while flag:
+		for event in pygame.event.get():
+			if (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN) or event.type == pygame.MOUSEBUTTONDOWN:
+				flag = False
+			elif event.type == pygame.KEYDOWN:
+				if event.key < 256 and chr(event.key) in '1234567890':
+					rect = pygame.Rect(12*m,25*n,100,100)
+					pygame.draw.rect(surface,(0,0,0),rect,0)
+					pygame.display.update()
+					depth = int(chr(event.key))
+					text = font.render(str(depth),1,(255,255,0))
+					textpos = text.get_rect(centerx = 25*n, centery = 25*m)
+					surface.blit(text, textpos)
+					pygame.display.update()
+    return depth
 
 def escolher_jogador():
     flag = True
