fed004e548b1e81c5be305e10ab983d87b56d24d
VICTOR EDUARDO BORGES DE ARAUJO 115210597 <victoreba@lcc3-041.lcc.ufcg.edu.br>
2018-08-03 12:27:09 -0300

    Alterações no adhoc

--- adhoc.py
+++ adhoc.py
@@ -1,33 +1,29 @@
 from base import *
+from random import shuffle, randint, choice
+
+def gerar_cantos(tabuleiro):
+    c = tabuleiro.colunas
+    l = tabuleiro.linhas
+    cantos = [(0,0), (0, c - 1), (l - 1, 0), (l - 1, c - 1)]
+    return cantos
 
 def alcancar_cantos(tabuleiro):
-    if tabuleiro[(0,0)] <= 0:
-        if checar_massa_critica(tabuleiro, (0,0)):
-            return (0,0)
-    elif tabuleiro[(0,5)] <= 0:
-        if checar_massa_critica(tabuleiro, (0,5)):
-            return (0,5)
-    elif tabuleiro[(8,0)] <= 0:
-        if checar_massa_critica(tabuleiro, (8,0)):
-            return (8,0)
-    elif tabuleiro[(8,5)] <= 0:
-        if checar_massa_critica(tabuleiro, (8,5)):
-            return (8,5)
-    else:
-        return cantos_bloqueados(tabuleiro)
+    cantos = gerar_cantos(tabuleiro)
+    shuffle(cantos)
+    for pos in cantos:
+        if(tabuleiro[pos] <= 0):
+            return pos
+    return outras_posicoes(tabuleiro)
 
-def checar_massa_critica(tabuleiro, pos):
-    if Tabuleiro.massa_critica(tabuleiro, pos) == abs(tabuleiro[pos]) :
-        return False
-    else:
-        return True
-
-def cantos_bloqueados(tabuleiro):
+def outras_posicoes(tabuleiro):
+    posicoes_livres = []
     for pos in [(x, y) for x in xrange(tabuleiro.linhas) for y in xrange(tabuleiro.colunas)]:
-        if tabuleiro(pos) <= 0:
-            if checar_massa_critica(tabuleiro, pos):
-                return pos
-    return (-1,-1)
+        if (tabuleiro[pos] <= 0):
+            posicoes_livres.append(pos)
+    return choice(posicoes_livres)
 
 def adhoc(tabuleiro):
+    n_magico = randint(0, 1)
+    if(n_magico == 0):
+        return outras_posicoes(tabuleiro)
     return alcancar_cantos(tabuleiro)
